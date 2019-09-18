6857af08ee6fbe43e4204379c720b020b4f20153
David Velasquez <dvelas25@eafit.edu.co>
2019-09-18 09:48:53 +0000

    Added .gitignore and removed systemd logger

--- run_bokeh.py
+++ run_bokeh.py
@@ -1,3 +1,6 @@
+import logging
+# from systemd.journal import JournaldLogHandler
+
 from numpy.random import random
 from bokeh.io import curdoc
 from bokeh.plotting import figure
@@ -7,6 +10,26 @@ from bokeh.server.server import Server
 
 """
 create and run a demo bokeh app on a cloud server
+"""
+
+"""
+Logging
+"""
+# # get an instance of the logger object this module will use
+# logger = logging.getLogger(__name__)
+# # instantiate the JournaldLogHandler to hook into systemd
+# journald_handler = JournaldLogHandler()
+# # set a formatter to include the level name
+# journald_handler.setFormatter(logging.Formatter(
+#     '[%(levelname)s] %(message)s'
+# ))
+# # add the journald handler to the current logger
+# logger.addHandler(journald_handler)
+
+# # optionally set the logging level
+# logger.setLevel(logging.DEBUG)
+"""
+End log
 """
 
 def run(doc):
@@ -27,9 +50,16 @@ def run(doc):
     click()
 
 # configure and run bokeh server
-kws = {'port': 5006, 'prefix': '/bokeh', 'allow_websocket_origin': ['edar40.tk']}
+# kws = {'port': 5006, 'prefix': '/bokeh', 'allow_websocket_origin': ['34.89.113.193','34.89.113.193:5006','34.89.113.193:5000']}
+kws = {'port': 5006, 'prefix': '/bokeh', 'allow_websocket_origin': ['34.89.113.193']}
+
 server = Server(run, **kws)
 server.start()
+
 if __name__ == '__main__':
+    # logger.info(
+    #     'Starting Bokeh Server on http://localhost:5006/bokeh'
+    # )
+    # print()
     server.io_loop.add_callback(server.show, '/')
     server.io_loop.start()
